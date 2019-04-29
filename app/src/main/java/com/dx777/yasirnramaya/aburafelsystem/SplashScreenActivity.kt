f0829aa9fea22a557f8c9dc6bf265ebae5dc5fc4
yasirn.ramaya <yasserdghx@gmail.com>
2019-04-29 19:55:27 +0300

    fix para

--- app/src/main/java/com/dx777/yasirnramaya/aburafelsystem/SplashScreenActivity.kt
+++ app/src/main/java/com/dx777/yasirnramaya/aburafelsystem/SplashScreenActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import com.chibatching.kotpref.Kotpref
 import com.dx777.yasirnramaya.aburafelsystem.login.LoginActivity
 
 class SplashScreenActivity : AppCompatActivity() {
@@ -11,6 +12,7 @@ class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+        Kotpref.init(this)
 
         Handler().postDelayed({
 
